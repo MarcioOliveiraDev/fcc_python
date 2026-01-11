627c6dd9331e107073b43e784de318fc9387a480
Márcio Brito <marcioyoung90@gmail.com>
2026-01-11 00:43:32 -0300

    Create a number_pattern from freeCodeCamp

---
+++ number_pattern.py
@@ -0,0 +1,19 @@
+def number_pattern(n):
+    result = ""
+
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+        
+    if n <= 0:
+        return "Argument must be an integer greater than 0."
+    
+    for number in range(1,n+1):
+        result += str(number) + " "
+    return result.strip()
+        
+    
+
+    
+
+print(number_pattern(4))
+print(number_pattern(12))
